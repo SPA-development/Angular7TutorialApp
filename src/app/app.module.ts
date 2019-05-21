30a909ee14cebfdb9a353c06fe98dc56b469eec9
Jef DeWitt <jefdewitt@gmail.com>
2019-05-20 21:08:17 -0400

    Adds recipe model class & template pieces to recipe-list.comp.html.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FailureComponent } from './alerts/failure-alert.component';
-import { SuccessComponent } from './alerts/success-alert.component';
-import { WarningComponent } from './alerts/warning-alert.component';
-import { BindingExamplesComponent } from './binding-examples/binding-examples.component';
-import { ToggleComponent } from './toggle/toggle.component';
-import { AlertContainerComponent } from './alert-container/alert-container.component';
+import { FailureComponent } from './earlierWork/alerts/failure-alert.component';
+import { SuccessComponent } from './earlierWork/alerts/success-alert.component';
+import { WarningComponent } from './earlierWork/alerts/warning-alert.component';
+import { BindingExamplesComponent } from './earlierWork/binding-examples/binding-examples.component';
+import { ToggleComponent } from './earlierWork/toggle/toggle.component';
+import { AlertContainerComponent } from './earlierWork/alert-container/alert-container.component';
+import { HeaderComponent } from './udemy-project-one/header/header.component';
+import { ShoppingListComponent } from './udemy-project-one/shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './udemy-project-one/recipe-book/recipe-book.component';
+import { ShoppingListItemComponent } from './udemy-project-one/shopping-list/shopping-list-item/shopping-list-item.component';
+import { ShoppingListEditComponent } from './udemy-project-one/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './udemy-project-one/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './udemy-project-one/recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './udemy-project-one/recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +26,15 @@ import { AlertContainerComponent } from './alert-container/alert-container.compo
     WarningComponent,
     BindingExamplesComponent,
     ToggleComponent,
-    AlertContainerComponent
+    AlertContainerComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    ShoppingListItemComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/udemy-project-one/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-recipe-detail',
+    template: '<p>app-recipe-detail works!'
+})
+export class RecipeDetailComponent {
+    
+    constructor() {}
+}
